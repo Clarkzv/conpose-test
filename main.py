dd8f3e9802b40ee7e3654c44bfd2b6d4f1e5b010
Clarkzv <112472600+Clarkzv@users.noreply.github.com>
2023-08-04 03:20:43 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import mido
+from pydub import AudioSegment
 import time
+import subprocess
 
 # Define your chords and their durations (in seconds)
 chords = [
     ("Em", 1),
     ("Cmaj7", 1),
     ("G", 1),
-    ("D_F#", 1),
+    ("D/F#", 1),
     ("Em", 1),
     ("Cmaj7", 1),
     ("G", 1),
@@ -17,14 +18,14 @@ chords = [
     ("Em", 1),
     ("Cmaj7", 1),
     ("G", 1),
-    ("D_F#", 1),
+    ("D/F#", 1),
     ("Am", 1),
     ("G", 1),
     ("D", 1),
     ("Em", 1),
     ("Cmaj7", 1),
     ("G", 1),
-    ("D_F#", 1),
+    ("D/F#", 1),
     ("Am", 1),
     ("G", 1),
     ("D", 1),
@@ -35,11 +36,11 @@ chords = [
     ("Em", 1),
     ("Cmaj7", 1),
     ("G", 1),
-    ("D_F#", 1),
+    ("D/F#", 1),
     ("Em", 1),
     ("Cmaj7", 1),
     ("G", 1),
-    ("D_F#", 1),
+    ("D/F#", 1),
     ("Em", 1),
     ("Cmaj7", 1),
     ("G", 1),
@@ -55,7 +56,7 @@ piano_notes = {
     "Cmaj7": ["C4", "E4", "G4", "B4"],
     "Em": ["E3", "G3", "B3", "E4"],
     "G": ["G3", "B3", "D4", "G4"],
-    "D_F#": ["D3", "F#3", "A3", "D4"],
+    "D/F#": ["D3", "F#3", "A3", "D4"],
 }
 
 # Function to convert note name to MIDI note number
@@ -66,27 +67,29 @@ def note_to_number(note):
     note_name, octave = note[:-1], int(note[-1])
     return (octave + 1) * 12 + notes_dict[note_name]
 
-# Function to create a chord MIDI messages
+# Function to create a chord AudioSegment
 def create_chord(chord_name, duration):
     notes = piano_notes.get(chord_name)
     if not notes:
         raise ValueError(f"Invalid chord name: {chord_name}")
     
-    note_msgs = [mido.Message('note_on', note=note_to_number(note), velocity=64) for note in notes]
-    chord_end = mido.Message('note_off', note=note_to_number(notes[0]), velocity=0, time=int(duration * 1000))
-    return note_msgs + [chord_end]
+    chord_audio = AudioSegment.empty()
+    for note in notes:
+        midi_note = note_to_number(note)
+        note_audio = AudioSegment.from_file(f"piano_samples/{midi_note}.wav")
+        chord_audio += note_audio
 
-# Create a MIDI file for the entire song
-mid = mido.MidiFile()
-track = mido.MidiTrack()
-mid.tracks.append(track)
+    silence = AudioSegment.silent(duration=int(duration * 1000))
+    return chord_audio + silence
+
+# Create the entire song as an AudioSegment
+song = AudioSegment.empty()
 for chord, duration in chords:
-    chord_msgs = create_chord(chord, duration)
-    for msg in chord_msgs:
-        track.append(msg)
+    chord_audio = create_chord(chord, duration)
+    song += chord_audio
 
-# Export the MIDI file
-midi_filename = "your_song.mid"
-mid.save(midi_filename)
+# Export the song as a WAV file
+wav_filename = "your_song.wav"
+song.export(wav_filename, format="wav")
 
-print(f"MIDI file '{midi_filename}' created. You can use MIDI-compatible software to listen or further edit the song.")
+print(f"WAV file '{wav_filename}' created. You can listen to or further edit the song.")
